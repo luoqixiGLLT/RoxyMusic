4fb6cccf61105729baf0539f5fc9c74c71af34d2
jxy2004 <2436345320@QQ.COM>
2024-06-14 10:52:07 +0800

    1

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -2,6 +2,7 @@ import {Fragment} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Index from "./Index.jsx";
 import Login from "./Login.jsx";
+import Search from "./Search.jsx";
 import Home from "./Index/Home.jsx";
 import Play from "./Index/Play.jsx";
 import User from "./Index/User.jsx";
@@ -12,11 +13,12 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Index/>}>
-                        <Route path='home' element={<Home/>} />
+                        <Route path='home' element={<Home/>} ></Route>
                         <Route path='play' element={<Play/>} />
                         <Route path='user' element={<User/>} />
                     </Route>
                     <Route path='/login' element={<Login/>}/>
+                    <Route path='/search' element={<Search/>}/>
                 </Routes>
             </BrowserRouter>
         </Fragment>
--- cliente/src/Index/Home.jsx
+++ cliente/src/Index/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './home.css'
+import { Button, SearchBar, Space, Toast } from 'antd-mobile'
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            首页12354756489
+        <div className='home-container'>
+            <div className='home-top'>
+                <input placeholder=' 搜索你想听的歌曲' onFocus={()=>{navigate('/search')}} className='home-inp'></input>
+                <img src='./2.png' className='home-img'></img>
+            </div>
+            
         </div>
     );
 };
