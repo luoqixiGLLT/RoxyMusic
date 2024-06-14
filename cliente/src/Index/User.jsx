0846ee9d630731c7a8e888b90c8fb90b8337b6ae
HunYaChao <2560393538@qq.com>
2024-06-14 09:52:46 +0800

    1

--- cliente/src/Index/User.jsx
+++ cliente/src/Index/User.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import { RightOutline } from 'antd-mobile-icons'
+import "./User.css"
 const User = () => {
     return (
         <div>
-            <div>
-                <div>
-                    <img src="../../1.jpeg" alt=""></img>
-                    <p>小音波</p>
+            <div className='User_body'>
+                <div className='User_text'>
+                    <div>
+                        <img src="../../1.jpeg" alt="" className='User_img'></img>
+                        <p className='User_p'>小音波1ETL7CX</p>
+                    </div>
+                    <div className='User_left' >
+                        <RightOutline />
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
