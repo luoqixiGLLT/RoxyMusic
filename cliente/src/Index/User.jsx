8c712ca7d285f59c81cb0cf18071515738a080a4
wwwonee <115781321+wwwonee@users.noreply.github.com>
2024-06-14 11:13:18 +0800

    1

--- cliente/src/Index/User.jsx
+++ cliente/src/Index/User.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { RightOutline, MailOutline, AppOutline } from 'antd-mobile-icons'
 import { Tabs } from 'antd-mobile'
+import { Carousel } from 'antd';
 import "./User.css"
+const contentStyle = {
+    margin: 0,
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
 const User = () => {
     return (
-        <div>
+        <div className='User_one'>
             <div className='User_body'>
                 <div className='User_top'>
                     <MailOutline fontSize={28} />
@@ -22,9 +31,10 @@ const User = () => {
                 </div>
                 <div className='User_cente'>
                     <div className='User_cente_one'>会员中心</div>
+                    <span className='User_cente_two'>|</span>
                     <div>活动中心</div>
                 </div>
-                <Tabs defaultActiveKey='1'>
+                <Tabs defaultActiveKey='1' className='tab'>
                     <Tabs.Tab title='音乐' key='1'>
 
                     </Tabs.Tab>
@@ -47,6 +57,22 @@ const User = () => {
                         7
                     </Tabs.Tab>
                 </Tabs>
+            </div>
+            <div>
+                <Carousel autoplay>
+                    <div>
+                        <h3 style={contentStyle}>1</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>2</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>3</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>4</h3>
+                    </div>
+                </Carousel>
             </div>
 
         </div>
