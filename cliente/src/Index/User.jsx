7ca1307e4d73ad47e747cd3ff55d08f72fd92695
HunYaChao <2560393538@qq.com>
2024-06-14 10:28:05 +0800

    777

--- cliente/src/Index/User.jsx
+++ cliente/src/Index/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RightOutline, MailOutline, AppOutline } from 'antd-mobile-icons'
+import { Tabs } from 'antd-mobile'
 import "./User.css"
 const User = () => {
     return (
@@ -20,9 +21,32 @@ const User = () => {
                     </div>
                 </div>
                 <div className='User_cente'>
-                    <div></div>
-                    <div></div>
+                    <div className='User_cente_one'>会员中心</div>
+                    <div>活动中心</div>
                 </div>
+                <Tabs defaultActiveKey='1'>
+                    <Tabs.Tab title='音乐' key='1'>
+
+                    </Tabs.Tab>
+                    <Tabs.Tab title='好友' key='2'>
+                        2
+                    </Tabs.Tab>
+                    <Tabs.Tab title='密友时刻' key='3'>
+                        3
+                    </Tabs.Tab>
+                    <Tabs.Tab title='话题' key='4'>
+                        4
+                    </Tabs.Tab>
+                    <Tabs.Tab title='作者' key='5'>
+                        5
+                    </Tabs.Tab>
+                    <Tabs.Tab title='喜欢' key='6'>
+                        6
+                    </Tabs.Tab>
+                    <Tabs.Tab title='动态' key='7'>
+                        7
+                    </Tabs.Tab>
+                </Tabs>
             </div>
 
         </div>
