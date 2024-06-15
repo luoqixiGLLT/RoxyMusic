79c93092c493a5bceac807170b7dc5189c6d0821
wwwonee <115781321+wwwonee@users.noreply.github.com>
2024-06-15 11:47:38 +0800

    5

--- cliente/src/Index/sou.jsx
+++ cliente/src/Index/sou.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './sou.css'
+import { SearchOutlined } from '@ant-design/icons'
 export default function sou() {
+  const list = [
+    { name: '若月亮还没来', new: 0, hot: 1 },
+    { name: '如果爱忘了', new: 0, hot: 0 },
+    { name: '承诺', new: 0, hot: 0 },
+    { name: '原来', new: 0, hot: 1 },
+    { name: '人间半途', new: 1, hot: 0 },
+    { name: '冬眠', new: 0, hot: 0 },
+    { name: 'aespa', new: 0, hot: 0 },
+    { name: '暮色回响', new: 0, hot: 0 },
+    { name: '表白', new: 0, hot: 0 },
+  ]
+  const [name, setName] = useState(JSON.parse(localStorage.getItem('name')))
+  const en = (e) => {
+    if (e.keyCode === 13) {
+      setName('')
+      localStorage.setItem('name', JSON.stringify(name))
+      getlists()
+    }
+
+  }
+  const [lists, setLists] = useState([])
+  const getlists = () => {
+    let arr = [...lists]
+    let names = localStorage.getItem('name')
+    let namess = JSON.parse(names)
+    list.forEach(item => {
+      if (item.name.includes(namess)) {
+        arr.splice(0, 1, item)
+      }
+    })
+    setLists(arr)
+  }
+  useEffect(() => {
+    getlists()
+  }, [])
   return (
-    <div className='Sou_box'> sou</div>
+    <div className='Sou_box'>
+      <div className='Search_box2'>
+        <div className='Search_box1'>
+          <SearchOutlined className='Search_sou' /><input placeholder='若月亮没来' value={name} onChange={(e) => { setName(e.target.value) }} onKeyDown={en} className='Search_input'></input>
+        </div>
+        <button className='Search_but'>取消</button>
+      </div>
+      <div>
+        {lists.map(item => {
+          return <p>{item.name}</p>
+        })}
+      </div>
+    </div>
   )
 }
