c14703651f8d6172e950411129b916551717a1f3
wwwonee <115781321+wwwonee@users.noreply.github.com>
2024-06-15 10:34:40 +0800

    5

--- cliente/src/Search.jsx
+++ cliente/src/Search.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import './Search.css'
+import { SearchOutlined } from '@ant-design/icons'
+import { DeleteOutline,RedoOutline } from 'antd-mobile-icons'
 export default function Search() {
+  const list=[
+    {name:'若月亮还没来',new:0,hot:1},
+    {name:'如果爱忘了',new:0,hot:0},
+    {name:'承诺',new:0,hot:0},
+    {name:'原来',new:0,hot:1},
+    {name:'人间半途',new:1,hot:0},
+    {name:'冬眠',new:0,hot:0},
+    {name:'aespa',new:0,hot:0},
+    {name:'暮色回响',new:0,hot:0},
+    {name:'表白',new:0,hot:0},
+  ]
+  const [history,setHistory] = useState([])
+  const [name,setName] = useState('')
+  const en=(e)=>{
+    if(e.keyCode===13){
+      setName('')
+      let historys = [...history]
+      historys.push(name)
+      setHistory(historys)
+      console.log(historys);
+    }
+
+  }
   return (
-    <div>搜索页面</div>
+    <div className='Search_box'>
+      <div className='Search_box2'>
+        <div className='Search_box1'>
+          <SearchOutlined className='Search_sou' /><input placeholder='若月亮没来' value={name} onChange={(e)=>{setName(e.target.value)}} onKeyDown={en} className='Search_input'></input>
+        </div>
+        <button className='Search_but'>取消</button>
+      </div>
+      <div className='Search_box4'>
+        <div className='Search_box4_1'>
+          <img src='./6.png' className='Search_img'></img>
+          <p className='Search_p1'>歌手</p>
+        </div>
+        <div className='Search_box4_1'>
+          <img src='./7.png' className='Search_img'></img>
+          <p className='Search_p1'>识曲</p>
+        </div>
+        <div className='Search_box4_1'>
+          <img src='./8.png' className='Search_img'></img>
+          <p className='Search_p1'>曲风</p>
+        </div>
+      </div>
+      <div className='Search_box5'>
+        <div className='Search_box5_1'>
+          <p className='Search_box5_p'>历史记录</p>
+          <DeleteOutline className='Search_del' />
+        </div>
+        <div>
+          {history.map(item=>{
+            return <button className='Search_box5_but'>{item}</button>
+          })}
+        </div>
+      </div>
+      <div className='Search_box5'>
+        <div className='Search_box5_1'>
+          <p className='Search_box5_p'>大家都在搜</p>
+          <RedoOutline className='Search_del' />
+        </div>
+        <div className='Search_box5_2'>
+          {list.map(item=>{
+            return <div className='Search_box5_3'>
+              <button className='Search_box5_but'>{item.hot===1?'🔥':''}{item.new===1?'🆕':''} {item.name}</button>
+            </div>
+            
+          })}
+         
+        </div>
+      </div>
+    </div>
   )
 }
