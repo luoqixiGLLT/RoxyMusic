43ce91e260c27b6e433143dbdd8bdd973da9507f
HunYaChao <2560393538@qq.com>
2024-06-16 16:45:38 +0800

    6556

--- cliente/src/Index/User/Talks.jsx
+++ cliente/src/Index/User/Talks.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import { Empty } from 'antd-mobile'
 export default function Talks() {
   return (
-    <div>Talks</div>
+    <div><Empty
+      style={{ padding: '64px 0' }}
+      imageStyle={{ width: 128 }}
+      description='暂无数据'
+    /></div>
   )
 }
